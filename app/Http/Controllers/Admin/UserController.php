90bfabc8c831f72dab179ceab9b23b9c5daf84b9
Do Hanh <dovanhanh12b@gmail.com>
2019-12-06 15:04:16 +0700

    ..

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -12,13 +12,14 @@ class UserController extends Controller
 {
     public function register(UserRequest $request)
     {
-        User::create([
+        $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => $request->password,
         ]);
 
         return response()->json([
+            'data' => $user,
             'message' => 'create success',
         ], 200);
     }
