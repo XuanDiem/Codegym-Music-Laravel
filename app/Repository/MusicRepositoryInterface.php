858d58d293fe02024487cc33536035b42ba26fdb
XuanDiem <xuandiem0011@gmail.com>
2019-12-22 05:38:41 +0700

    edit a song

---
+++ app/Repository/MusicRepositoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Repository;
+
+
+use Illuminate\Http\Request;
+
+interface MusicRepositoryInterface
+{
+    public function getMusics();
+
+    public function create($music);
+
+    public function finById($id);
+
+    public function update($music);
+
+    public function delete($idMusic);
+}
